c1a2788e9a29bd0456dc81165647c56c5bb777d5
Sai-hub-lang <saisjadhav7@gmail.com>
2025-07-07 09:37:29 +0530

    Day 2

---
+++ Day2/src/Breakdemo.java
@@ -0,0 +1,19 @@
+
+public class Breakdemo {
+	
+	public static void main(String[] args) {
+		
+		for(int i=1; i<10; i++)
+		{
+			
+				//break;
+				System.out.println(i);
+	
+			System.out.println("Hello friends");
+			break;
+			
+		}
+
+	}
+
+}
